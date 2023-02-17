4d3cca8733efa4011b69375a2bebadeff6b5bc04
DuduzileR <duduzileratsuma@gmail.com>
2023-02-17 05:57:54 -0800

    my first c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * Main - print a line of code using puts
- *
- * Return: 0
+/* *
+ *main - Entry point 
+ *return: Always 0 (sucess)
  */
 int main (void)
 {
-	("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
