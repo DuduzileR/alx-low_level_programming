23abd96758937c6631c96edeadd1c20e6c239224
DuduzileR <duduzileratsuma@gmail.com>
2023-02-17 00:00:44 -0800

    my first line of c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Main - print a line of code using puts
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
