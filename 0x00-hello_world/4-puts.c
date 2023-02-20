c0300624aa2016e043204fce5293ef85135b207a
DuduzileR <duduzileratsuma@gmail.com>
2023-02-20 03:30:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/* *
- *main - Entry point 
- *return: Always 0
+/**
+ * main - program that prints line with puts function
+ * return: 0
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
