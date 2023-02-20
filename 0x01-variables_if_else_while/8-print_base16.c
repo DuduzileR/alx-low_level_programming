a69a8bdccb8dab12e04bb18b338e27bd76cd5830
DuduzileR <duduzileratsuma@gmail.com>
2023-02-20 08:21:16 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - prints all the numbers of base 16 in lowercase,
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - prints all all possible combinations of single digit numbers
+ * Return: Always 0
  */
 int main(void)
 {
 	int n;
-	char ch;
 
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
 }
-
