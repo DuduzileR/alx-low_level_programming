b68b5ebaa775a3962a0cf0306bbca9dd33967017
DuduzileR <duduzileratsuma@gmail.com>
2023-02-22 07:23:30 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- *main - Entry point
- *
- *Return: nothing, return void
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
 {
@@ -15,6 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
